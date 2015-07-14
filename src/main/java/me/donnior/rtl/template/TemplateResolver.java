1b198f07f67ca4e6145c822ab7de027d7b18601d
Donny <donnior@gmail.com>
2015-07-14 14:45:19 +0800

    prepare for agilej

--- src/main/java/me/donnior/rtl/template/TemplateResolver.java
+++
@@ -1,9 +0,0 @@
-package me.donnior.rtl.template;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface TemplateResolver {
-	
-	String resolveTemplateName(HttpServletRequest request);
-
-}
